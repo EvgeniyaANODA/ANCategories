213664d77df2d202e8e91791f809423265ca7f04
Oksana Kovalchuk <oksana@anoda.mobi>
2014-11-23 16:02:21 +0200

    update font category

--- UIFont/UIFont+ANAdditions.h
+++ UIFont/UIFont+ANAdditions.h
@@ -7,9 +7,22 @@
 
 @interface UIFont (ANAdditions)
 
-+ (UIFont *)an_lightFontWithSize:(CGFloat)size;
 + (UIFont *)an_ultraLightFontWithSize:(CGFloat)size;
-+ (UIFont *)an_mediumFontWithSize:(CGFloat)size;
-+ (UIFont *)an_normalFontWithSize:(CGFloat)size;
++ (UIFont *)an_lightFontWithSize:(CGFloat)size;
++ (UIFont *)an_regularFontWithSize:(CGFloat)size;
++ (UIFont *)an_meduimFontWithSize:(CGFloat)size;
++ (UIFont *)an_semiboldFontWithSize:(CGFloat)size;
++ (UIFont *)an_boldFontWithSize:(CGFloat)size;
++ (UIFont *)an_condensedBlackFontWithSize:(CGFloat)size;
++ (UIFont *)an_condensedBoldFontWithSize:(CGFloat)size;
+
+#pragma mark - Italic
+
++ (UIFont *)an_italicUltraLightFontWithSize:(CGFloat)size;
++ (UIFont *)an_italicLightFontWithSize:(CGFloat)size;
++ (UIFont *)an_italicRegularFontWithSize:(CGFloat)size;
++ (UIFont *)an_italicMeduimFontWithSize:(CGFloat)size;
++ (UIFont *)an_italicSemiboldFontWithSize:(CGFloat)size;
++ (UIFont *)an_italicBoldFontWithSize:(CGFloat)size;
 
 @end
